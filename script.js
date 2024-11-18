42c175d3fb2b7ea889c8003d80b6c0a50e0eb499
nicolemagdz <nicolemmagdziasz@lewisu.edu>
2024-11-17 23:16:57 -0600

    Added button to fetch API

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
-function fetchData() {
+function fetchDataThen() {
     fetch('https://https://node-api-server-ap-fagsgte7fsdqhyct.centralus-01.azurewebsites.net/api/data')
     .then(response => response.json())
     .then(data => {
         document.getElementById('output').textContent = JSON.stringify(data);
     })
     .catch(error => console.error('Error:', error));
+}
+
+async function fetchData() {
+    const apiUrl = "https://node-api-server-ap-fagsgte7fsdqhyct.centralus-01.azurewebsites.net/api/date";
+
+    try {
+        const response = await fetch(apiUrl);
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const data = await response.json();
+        document.getElementById("output").textContent = `Current Date and Time: ${date.dateTime}`;
+    } catch (error) {
+        console.error("Error fetching data:", error);
+        document.getElementById("output").textContent = "Failed to fetch data from the server.";
+    }
 }
